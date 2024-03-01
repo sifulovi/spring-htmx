3824438c95eeb4486756d58cf7d8c1709b12f6df
Ove <isiful.ovi@gmail.com>
2024-03-02 02:58:14 +0600

    Added date range

--- src/main/java/com/sio/springhtmx/TodoController.java
+++ src/main/java/com/sio/springhtmx/TodoController.java
@@ -1,8 +1,9 @@
 package com.sio.springhtmx;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Controller;
@@ -10,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-record Todo(int id, String title, String status) {
+record Todo(int id, String title, String status, LocalDate date) {
 }
 
 @Controller
@@ -21,26 +22,36 @@ public class TodoController {
     static {
         IntStream.range(1, 50).forEach(
                 it -> {
-                    var todo = new Todo(it, "Todo - " + it, it % 2 == 0 ? "Active" : "Inactive");
+                    var todo = new Todo(it, "Todo - " + it, it % 2 == 0 ? "Active" : "Inactive",
+                            LocalDate.now().plusDays(it));
                     STORE.add(todo);
                 });
     }
 
-    @GetMapping()
+    @GetMapping("/")
     public String list(Model model) {
         model.addAttribute("todos", STORE);
+        model.addAttribute("startDate", LocalDate.now());
+        model.addAttribute("endDate", LocalDate.now().plusDays(7));
         return "todo/todo-list";
     }
 
     @GetMapping("/filters")
     public String todoList(
-            @RequestParam(required = false) String title,
-            @RequestParam(required = false) String status,
+            @RequestParam(required = false, defaultValue = "") String title,
+            @RequestParam(required = false, defaultValue = "") String status,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate,
             Model model) {
 
         var list = STORE.stream()
                 .filter(it -> title.isEmpty() || it.title().toLowerCase().startsWith(title.toLowerCase()))
                 .filter(it -> status.isEmpty() || it.status().toLowerCase().startsWith(status.toLowerCase()))
+                .filter(it -> (startDate == null || endDate == null)
+                        || (Objects.requireNonNullElse(it.date(), LocalDate.MIN)
+                                .isAfter(Objects.requireNonNullElse(startDate, LocalDate.MIN).minusDays(1))
+                                && Objects.requireNonNullElse(it.date(), LocalDate.MAX)
+                                        .isBefore(Objects.requireNonNullElse(endDate, LocalDate.MAX).plusDays(1))))
                 .toList();
 
         model.addAttribute("todos", list);
